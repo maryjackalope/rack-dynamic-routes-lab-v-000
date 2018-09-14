6f391c22acf164d1c200c65a0a93fd105cc2812a
Mary Jack <maryjackinthebox@gmail.com>
2018-09-14 19:12:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,5 +9,18 @@ class Application
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
     
+    if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+     resp.finish
+  end
+end 
   
   
