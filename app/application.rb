9abeffbe885979a8a0b523223c3b39f7eba3f18f
Mary Jack <maryjackinthebox@gmail.com>
2018-09-14 19:13:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,8 @@ class Application
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
     
+    item = @@items.find{|i| i.name == item_name}
+     
     if item.nil?
         resp.write "Item not found"
         resp.status = 400
